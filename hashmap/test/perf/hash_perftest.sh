04f74e3faf3f3ba10b1e62e7826b8cd7ab319667
Brendon Scheinman <bscheinman@gmail.com>
2012-12-16 20:01:34 -0600

    cleaned up hashmap insert performance test

--- hashmap/test/perf/hash_perftest.sh
+++ hashmap/test/perf/hash_perftest.sh
@@ -1,10 +1,14 @@
 #/usr/bin/sh
 
-ELEMENT_COUNT=2000000
+ELEMENT_COUNT=1000000
 
-for i in 32768 65536 131072 262144 524288 2000000
+make -C ../.. all
+
+for i in 131072 262144 524288 2000000
 do
-    echo "$ELEMENT_COUNT elements in $i bins"
-    time ./hash_perftest $i $ELEMENT_COUNT
-    time ./std_hash_test $i $ELEMENT_COUNT
+    echo "===== $ELEMENT_COUNT elements in $i bins ====="
+    echo -n "c-collection hash map:"
+    time ./hash_insert_test $i $ELEMENT_COUNT
+    echo -n "std::unordered_map:"
+    time ./std_hash_insert_test $i $ELEMENT_COUNT
 done
